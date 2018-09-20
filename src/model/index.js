48769c1853ba08f42e0796bbcd29161acf3bae5e
David Arenas <darensr@gmail.com>
2018-09-20 12:30:34 +0200

    refactor(model): add warning when priority string is wrong

--- src/model/index.js
+++ src/model/index.js
@@ -1,6 +1,7 @@
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 import { types } from 'mobx-state-tree';
 
-const priorityValues = {
+export const priorityValues = {
   high: 10,
   medium: 20,
   low: 30,
@@ -17,9 +18,20 @@ export default types
     },
   }))
   .actions(self => ({
-    addProcessor(processor, priority) {
-      const priorityValue =
-        typeof priority === 'string' ? priorityValues[priority] : priority;
+    addProcessor(processor, priority = 'low') {
+      let priorityValue;
+      if (typeof priority === 'string') {
+        priorityValue = priorityValues[priority];
+      } else if (typeof priority === 'number') {
+        priorityValue = priority;
+      }
+
+      if (!priorityValue) {
+        priorityValue = priorityValues.low;
+        console.warn(
+          `Wrong priority value (${priority}) for a processor. Must be a number, 'high', 'medium' or 'low'. Setting priority to 'low'.`,
+        );
+      }
 
       self.processors.push({ priority: priorityValue, ...processor });
     },
